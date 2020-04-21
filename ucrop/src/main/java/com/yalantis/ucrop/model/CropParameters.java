8dc0de1b5027c328197fa7b641a6b80077c3948e
t2314862168 <2314862168@qq.com>
2020-04-21 22:21:27 +0800

    first commit

---
+++ ucrop/src/main/java/com/yalantis/ucrop/model/CropParameters.java
@@ -0,0 +1,71 @@
+package com.yalantis.ucrop.model;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+/**
+ * Created by Oleksii Shliama [https://github.com/shliama] on 6/21/16.
+ */
+public class CropParameters {
+
+    private int mMaxResultImageSizeX, mMaxResultImageSizeY;
+
+    private Bitmap.CompressFormat mCompressFormat;
+    private int mCompressQuality;
+    private Uri mImageInputUri, mImageOutputUri;
+    private String mImageInputPath, mImageOutputPath;
+    private ExifInfo mExifInfo;
+
+
+    public CropParameters(int maxResultImageSizeX, int maxResultImageSizeY,
+                          Bitmap.CompressFormat compressFormat, int compressQuality,
+                          Uri imageInputUri, Uri imageOutputUri,
+                          String imageInputPath, String imageOutputPath, ExifInfo exifInfo) {
+        mMaxResultImageSizeX = maxResultImageSizeX;
+        mMaxResultImageSizeY = maxResultImageSizeY;
+        mCompressFormat = compressFormat;
+        mCompressQuality = compressQuality;
+        mImageInputUri = imageInputUri;
+        mImageOutputUri = imageOutputUri;
+        mImageInputPath = imageInputPath;
+        mImageOutputPath = imageOutputPath;
+        mExifInfo = exifInfo;
+    }
+
+    public int getMaxResultImageSizeX() {
+        return mMaxResultImageSizeX;
+    }
+
+    public int getMaxResultImageSizeY() {
+        return mMaxResultImageSizeY;
+    }
+
+    public Bitmap.CompressFormat getCompressFormat() {
+        return mCompressFormat;
+    }
+
+    public int getCompressQuality() {
+        return mCompressQuality;
+    }
+
+    public String getImageInputPath() {
+        return mImageInputPath;
+    }
+
+    public String getImageOutputPath() {
+        return mImageOutputPath;
+    }
+
+    public Uri getImageInputUri() {
+        return mImageInputUri;
+    }
+
+    public Uri getImageOutputUri() {
+        return mImageOutputUri;
+    }
+
+    public ExifInfo getExifInfo() {
+        return mExifInfo;
+    }
+
+}
